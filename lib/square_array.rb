811460b3b4a410330de2302280b1a183587b2621
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-07-13 00:00:44 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
 numbers = [1,2,3]
-square_array(numbers)
-new_numbers = [9,10,16,25]
-square_array(new_numbers)
-
+while numbers [counter]
+puts pets[counter]
+counter +=1
 end
